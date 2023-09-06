439647597b4e59b66119d00bcc290a23c54ab33e
bertonfederico <federicoberton.fb@gmail.com>
2023-09-06 12:37:03 +0200

    Changed height positions

--- js/firstPage.js
+++ js/firstPage.js
@@ -1,7 +1,7 @@
 var firstPageSvg;
 
 var firstPageWidth = window.innerHeight;
-var firstPageHeight = 550;
+var firstPageHeight = 555;
 
 function setUpFirstPage() {
     firstPageSvg = d3.select("#firstPageContainer").append("svg")

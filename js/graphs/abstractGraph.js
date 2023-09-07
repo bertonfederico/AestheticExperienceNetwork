3c08ce3183bfe040177494cdc66608d93fb4c10c
bertonfederico <federicoberton.fb@gmail.com>
2023-09-07 19:21:18 +0200

    Fixed wrong deletion

--- js/graphs/abstractGraph.js
+++ js/graphs/abstractGraph.js
@@ -13,7 +13,7 @@ class AbstractGraph {
         this.artworkContainer = this.hasTwoDropdown ? document.getElementById(this.tag + "Selector1") :  document.getElementById(this.tag + "Selector");
         let index = this.artworkContainer.options.length;
         while (index--) {
-            this.artworkContainer.remove(index);
+            if (this.artworkContainer.options[index].value != "All") this.artworkContainer.remove(index);
         }
         graphData.forEach((value, key) =>{
             const li = document.createElement("option");
